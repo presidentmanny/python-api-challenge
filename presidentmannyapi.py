a76d97ad7b3c85007f5b51215b184ba089e03608
presidentmanny <presidentmanny13@gmail.com>
2019-11-13 12:49:48 -0500

    Add files via upload

---
+++ presidentmannyapi.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "zMIriEOEdJxO6rNop60Khdc88"
+consumer_secret = "oT58Y1VAPtis9S5fLt0K9LeWDxGNOUUocDansAfYZUdrnakRTQ"
+access_token = "1186404732547547137-0wyqvKDMLF431dhKvP1eYETYEHFyk6 "
+access_token_secret = "F6Ezy3D3wkaqPtHgsP62uhXJ4uZPUoAbyTSUsJKt1ffdg"
